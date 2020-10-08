7d72fea0e8e826f25c355e1ec59895e461ddb7db
Coni <conixat@gmail.com>
2020-10-08 13:18:40 +0800

    testing requests

--- tbbc.py
+++ tbbc.py
@@ -1,7 +1,22 @@
 # tbbc.py
 import time
+import requests
+
+# Get key from apikey.txt
+key = ""
+with open("apikey.txt","r") as f:
+    key = f.readline().strip()
+
+# Test if it works
+url = "https://osu.ppy.sh/p/api/get_user_best"
+params = {"k": key, "u": 2188855, "m": 0, "limit": 10, "type": "id"}
+
+resp = requests.get(url, params)
+data = resp.json()
+
+print(data)
 
 # Main loop
 while True:
-    
-    wait(2) # Avoid spamming requests
+
+    time.sleep(2) # Avoid spamming requests
